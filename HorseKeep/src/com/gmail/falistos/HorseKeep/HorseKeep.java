f6a16c53222908380d043c9e976b9cfdf69a153d
Ken Piper <kealper@gmail.com>
2016-02-09 22:39:21 -0600

    Bump version to correct one
    And apparently my IDE didn't like that padding with nothing trailing it

--- HorseKeep/src/com/gmail/falistos/HorseKeep/HorseKeep.java
+++ HorseKeep/src/com/gmail/falistos/HorseKeep/HorseKeep.java
@@ -22,7 +22,7 @@ import java.util.Map.Entry;
 
 public class HorseKeep extends JavaPlugin implements Listener
 {
-	public String version = "0.3.1";
+	public String version = "0.3.2";
 
 	public Permission perm = null;
 	public HorseManager manager = null;
@@ -30,28 +30,28 @@ public class HorseKeep extends JavaPlugin implements Listener
 	public HorseData data = null;
 
 	public String prefix = "HorseKeep";
-	
+
 	public void onEnable()
 	{
 		// Configuration
 		getConfig().options().copyDefaults(true);
 		saveDefaultConfig();
-		
+
 		// Chat prefix
 		if (getConfig().get("chatPrefix") != null)
 		{
 			this.prefix = (String) getConfig().get("chatPrefix");
 		}
-		
+
 		// Horses Data
 		this.data = new HorseData(this, "horses.yml");
 		this.data.migrate();
 		this.data.migrateUUID();
-		
+
 		// Locale
 		this.lang = new Locale(this, "language_"+getConfig().getString("language"),"language_en.yml");
 		lang.saveDefault();
-		
+
 		// Metrics
 		if (getConfig().getBoolean("enableMetrics"))
 		{
@@ -62,25 +62,25 @@ public class HorseKeep extends JavaPlugin implements Listener
 			    getLogger().info("Can't submit plugin usage data to Metrics");
 			}
 		}
-		
+
 		// Vault
 		if(Bukkit.getPluginManager().getPlugin("Vault") == null) {
 			getLogger().severe("Vault is not installed, this plugin require Vault");
 			Bukkit.getPluginManager().disablePlugin(this);
 			return;
 		}
-		
+
 		setupPermissions();
 
 		// Horses Manager
 		this.manager = new HorseManager(this);
-		
+
 		getServer().getPluginManager().registerEvents(this, this);
 		getLogger().info("Enabled");
 	}
 
 	public HorseData getHorseData() { return this.data; }
-	
+
 	public String getChatPrefix()
 	{
 		return ChatColor.RED + "[" + ChatColor.GOLD + this.prefix + ChatColor.RED + "] " + ChatColor.GREEN;
@@ -155,13 +155,13 @@ public class HorseKeep extends JavaPlugin implements Listener
 			}
 			else if (args[0].equalsIgnoreCase("admin"))
 			{
-				
+
 				if (!perm.has(sender, "horsekeep.admin"))
 				{
 					sender.sendMessage(this.getChatPrefix() + ChatColor.RED + this.lang.get("noPermission"));
 					return true;
 				}
-				
+
 				if (args.length < 2) return true;
 
 				if (args[1].equalsIgnoreCase("list"))
@@ -178,24 +178,24 @@ public class HorseKeep extends JavaPlugin implements Listener
 		}
 		return false;
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGH)
 	public void onEntityDamage(EntityDamageEvent e) {
-		
+
 		if (this.manager.isHorse(e.getEntity()))
 		{
 			LivingEntity horse = (LivingEntity) e.getEntity();
-			
+
 			if (this.manager.isOwned(horse.getUniqueId()))
 			{
 				if(e instanceof EntityDamageByEntityEvent)
 				{
 					EntityDamageByEntityEvent e1 = (EntityDamageByEntityEvent) e;
-					
+
 			        if (e1.getDamager() instanceof Player)
 			        {
 				        Player damager = (Player) e1.getDamager();
-				        
+
 				        if(!perm.has(damager, "horsekeep.bypass.protection"))
 				        {
 					        if(getConfig().getBoolean("disableHorseDamage"))
@@ -212,7 +212,7 @@ public class HorseKeep extends JavaPlugin implements Listener
 			        else if (e1.getDamager() instanceof Projectile)
 			        {
 			        	Projectile projectile = (Projectile) e1.getDamager();
-			        	
+
 				  	    if (projectile.getShooter() instanceof Player)
 					    {
 					        Player shooter = (Player) projectile.getShooter();
@@ -248,18 +248,18 @@ public class HorseKeep extends JavaPlugin implements Listener
 		}
 
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGH)
     public void PotionsSplash(PotionSplashEvent e){
         if(e.getEntity().getShooter() instanceof Player){
                 Player thrower = (Player) e.getEntity().getShooter();
                 Collection<LivingEntity> AffectedEntities = e.getAffectedEntities();
                 Iterator<LivingEntity> it = AffectedEntities.iterator();
-                
+
                 boolean cancelEvent = false;
-                
+
                 String message = null;
-                
+
                 while (it.hasNext())
                 {
                 	LivingEntity entity = it.next();
@@ -268,7 +268,7 @@ public class HorseKeep extends JavaPlugin implements Listener
                 		if(this.manager.isOwned(entity.getUniqueId()) && !perm.has(thrower, "horsekeep.bypass.protection"))
         				{
                     		if (getConfig().getBoolean("disableHorseDamage"))
-        	  	        	{		
+        	  	        	{
         	  	        		message = this.getChatPrefix() + this.lang.get("cantAttackOwnedHorse");
         	  	        		cancelEvent = true;
         	  	        	}
@@ -279,7 +279,7 @@ public class HorseKeep extends JavaPlugin implements Listener
         				}
                 	}
                 }
-                
+
                 if (cancelEvent)
                 {
                 	thrower.sendMessage(message);
@@ -287,7 +287,7 @@ public class HorseKeep extends JavaPlugin implements Listener
                 }
         }
     }
-	
+
 	@EventHandler(priority = EventPriority.HIGH)
     public void onDeath(EntityDeathEvent e) {
         Entity entity = e.getEntity();
@@ -299,19 +299,19 @@ public class HorseKeep extends JavaPlugin implements Listener
             	Player owner = Bukkit.getPlayer(ownerUUID);
 
             	if (owner.isOnline()) owner.sendMessage(this.getChatPrefix() + ChatColor.RED + this.lang.get("horseDead").replace("%id", this.manager.getHorseIdentifier(entity.getUniqueId())));
-            	
+
             	this.manager.removeHorse(this.manager.getHorseIdentifier(entity.getUniqueId()));
             }
         }
     }
 
-	
+
 	@EventHandler(priority = EventPriority.NORMAL)
     public void onChunkUnloaded(ChunkUnloadEvent event) {
         Chunk c = event.getChunk();
-        
+
         Entity[] entities = c.getEntities();
-        
+
         for(Entity e: entities){
         	if (this.manager.isHorse(e))
         	{
@@ -320,36 +320,36 @@ public class HorseKeep extends JavaPlugin implements Listener
         			// We save horse location
         			Location loc = e.getLocation();
         			this.data.getHorsesData().set("horses."+e.getUniqueId()+".lastpos",loc.getWorld().getName()+":"+loc.getX()+":"+loc.getY()+":"+loc.getZ()+":"+loc.getYaw()+":"+loc.getPitch());
-        			
+
         			this.data.save();
         		}
         	}
         }
     }
-	
+
     @EventHandler(priority = EventPriority.NORMAL)
     public void onEntityInteract(PlayerInteractEntityEvent event)
     {
       if (this.manager.isHorse(event.getRightClicked()))
       {
-    	
+
         Horse horse = (Horse) event.getRightClicked();
-        
+
         if (!this.manager.isOwned(horse.getUniqueId()))
         {
         	if (event.getPlayer().getItemInHand().getType() == Material.SADDLE)
-        	{       		
+        	{
         		if (!perm.has(event.getPlayer(), "horsekeep.protect") && !perm.has(event.getPlayer(), "horsekeep.admin"))
 				{
         			return;
 				}
-        		
+
         		List<Map<?, ?>> groups;
-        		
+
         		groups = getConfig().getMapList("groups");
-        		
+
         		int limit = getConfig().getInt("horsesDefaultLimit");
-        		
+
         		if (!perm.has(event.getPlayer(), "horsekeep.bypass.limit"))
 				{
             		for(Map<?, ?> item : groups){
@@ -362,7 +362,7 @@ public class HorseKeep extends JavaPlugin implements Listener
                 			}
             			}
             		}
-            		
+
             		if (this.manager.getOwnedHorses(event.getPlayer().getUniqueId()).size() >= limit)
             		{
             			event.getPlayer().sendMessage(this.getChatPrefix() + ChatColor.GOLD + this.lang.get("horsesLimitReached"));
@@ -371,15 +371,15 @@ public class HorseKeep extends JavaPlugin implements Listener
 				}
 
         		if (event.getPlayer().getName().isEmpty()) { event.getPlayer().sendMessage(this.getChatPrefix() + ChatColor.RED + "Error while setting up protection"); return; }
-        		
+
         		this.manager.setHorseOwner(event.getPlayer(), horse);
-        		
+
         		horse.getWorld().playSound(
-        		          horse.getLocation(), 
+        		          horse.getLocation(),
         		          Sound.LEVEL_UP, 10.0F, 1.0F);
-        		
+
         		event.getPlayer().sendMessage(this.getChatPrefix() + this.lang.get("horseProtected").replace("%id", this.manager.getHorseIdentifier(this.manager.getHorseUUID(horse))));
-        		
+
         		if (getConfig().getBoolean("autoTameHorseOnProtect")) { horse.setTamed(true); }
         	}
         }
@@ -390,23 +390,23 @@ public class HorseKeep extends JavaPlugin implements Listener
     			String ownerName = this.manager.getOwnerName(horse.getUniqueId());
 
 		    	if (ownerName == null) ownerName = "Unknown";
-		    	
+
             	event.getPlayer().sendMessage(this.getChatPrefix() + ChatColor.RED + this.lang.get("horseBelongsTo").replace("%player", ownerName));
-            	
+
             	if (perm.has(event.getPlayer(), "horsekeep.admin")) { event.getPlayer().sendMessage(this.getChatPrefix() + this.lang.get("identifier") + " " + this.manager.getHorseIdentifier(this.manager.getHorseUUID(horse))); }
-            	
-            	event.setCancelled(true);        		
+
+            	event.setCancelled(true);
         	}
         }
       }
     }
-    
+
     @EventHandler(priority = EventPriority.NORMAL)
     public void onAnimalTame(EntityTameEvent e)
     {
     	LivingEntity tamedAnimal = e.getEntity();
     	Player player = (Player) e.getOwner();
-    	
+
     	if (this.manager.isHorse(tamedAnimal) && !this.manager.isOwned(tamedAnimal.getUniqueId()))
     	{
     		player.sendMessage(this.getChatPrefix() + this.lang.get("tamedHorse"));
@@ -417,13 +417,13 @@ public class HorseKeep extends JavaPlugin implements Listener
     public void onVehicleEnter(VehicleEnterEvent event)
     {
 		if (!(event.getEntered() instanceof Player)) { return; }
-		
+
 		Player player = (Player) event.getEntered();
-		
+
 	    if (this.manager.isHorse(event.getVehicle()))
 	    {
 	    	Horse horse = (Horse) event.getVehicle();
-	    	
+
 	    	if (this.manager.isOwned(horse.getUniqueId()))
 	    	{
 	    		if (!this.manager.canMountHorse(player, horse) && !perm.has(player, "horsekeep.bypass.mount"))
@@ -431,9 +431,9 @@ public class HorseKeep extends JavaPlugin implements Listener
 	    			String ownerName = this.manager.getOwnerName(horse.getUniqueId());
 
 			    	if (ownerName == null) ownerName = "Unknown";
-			    		
+
 	    			player.sendMessage(this.getChatPrefix() + this.lang.get("horseBelongsTo").replace("%player", ownerName));
-	    			
+
 	    			event.setCancelled(true);
 	    		}
 	    	}
